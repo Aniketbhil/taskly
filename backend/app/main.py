236a5c8369a08cc355caaf19285efaffc8a869f4
Aniket Bhil <you@example.com>
2026-01-24 01:13:32 +0530

    Allow CORS from production frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ app = FastAPI(title= settings.PROJECT_NAME)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["*"],   # allow all for hackathon & production simplicity
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
